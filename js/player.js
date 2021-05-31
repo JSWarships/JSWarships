c27d359d345f9411168daa4d14c0c5c8d55193fd
Asor1k <roma.dotsenko@gmail.com>
2021-05-31 16:45:10 +0300

    added game states(filling, fighting)

--- js/player.js
+++ js/player.js
@@ -2,8 +2,14 @@
 
 const PlayerType = 
 {
-    Player1: 0,
-    Player2: 1
+    Player2: 0,
+    Player1: 1
+};
+
+const GameState = 
+{
+    FillingGrid: 0,
+    Fighting: 1
 };
 
 const shipAliveColor = "blue";
@@ -63,7 +69,7 @@ const fill_by_player = ( cell ) =>
         const add_cell_to_enviroment = (last_cell_position) => {
             player1.currentShip.add_cell ( cell );
             GameEnviroment.add_ship_cell ( cell, player1.player_type, last_cell_position );
-            GameEnviroment.drawRectangleWithPosition ( cell.localPosition, player1.player_type, shipAliveColor );
+            GameEnviroment.drawRectangleWithPosition ( cell.local_position, player1.player_type, shipAliveColor );
         };
 
         if( !player1.currentShip )
@@ -71,7 +77,7 @@ const fill_by_player = ( cell ) =>
             if( cell.cell_type == CellType.Empty )
             {
                 player1.currentShip = new Ship ( currShipType.shipSize );
-                add_cell_to_enviroment(null);
+                add_cell_to_enviroment ( null );
             }
             else return;
         };
@@ -79,7 +85,7 @@ const fill_by_player = ( cell ) =>
         if ( cell.cell_type == CellType.Potential )
         {
             const cels_in_ship = player_ship.cells.length;
-            add_cell_to_enviroment(player_ship.cells [cels_in_ship - 1].localPosition);
+            add_cell_to_enviroment(player_ship.cells [cels_in_ship - 1].local_position);
         };
         
         if ( player1.currentShip.cells.length == currShipType.shipSize )
@@ -97,19 +103,39 @@ const fill_by_player = ( cell ) =>
             console.log ( "next type" );
         };
     };
+};
+
+const fill_random = ( ) =>
+{
+    //fill random
+    finish_filling_grid( );
 }
 
-const onPlayerClick = ( e ) =>
+const onPlayerClick = ( mouse_pos ) =>
 {
-        const cell = GameEnviroment.findClickedCell ( e.pageX, e.pageY, player1.player_type );
+        const cell = GameEnviroment.findClickedCell ( mouse_pos.pageX, mouse_pos.pageY, player1.player_type );
         
         if ( !cell ) return;
-        if(player1.isFillingByPlayer)
+        switch (game_state)
         {
-            fill_by_player( cell );
+            case GameState.FillingGrid: 
+                if( player1.isFillingByPlayer )
+                {
+                    fill_by_player( cell );
+                }
+                else 
+                {
+                    fill_random ( );
+                }
+                break;
+            case GameState.Fighting:
+                player1.attack_cell ( cell );
+                break;
         }
-        //this.grid.add_ship ( new Ship ( cell.localPosition, ) );
-        //GameEnviroment.drawPoint ( cell.localPosition.x, cell.localPosition.y, PlayerType.Player2, 'black' );
+        
+
+        //this.grid.add_ship ( new Ship ( cell.local_position, ) );
+        //GameEnviroment.drawPoint ( cell.local_position.x, cell.local_position.y, PlayerType.Player2, 'black' );
 };
 
 class Player
@@ -135,7 +161,12 @@ class Player
         this.fillGridByPlayer( );
     };
 
-    fillGridByPlayer( )
+    attack_cell ( )
+    {
+        this.emiter.emit ( "PlayerAttacked", cell_position );
+    }
+
+    fillGridByPlayer ( )
     {
         this.isFillingByPlayer = true;
     };
@@ -145,6 +176,7 @@ class Player
         console.log ( "Filling is finished, starting the game..." );
         GameUI.textDrawer ( "Starting the game..." );
         GameUI.placeShipHide ( );
+        game_state = GameState.Fighting;
         this.isFillingByPlayer = false;
     };
 
