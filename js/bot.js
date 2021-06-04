1e87f63247442a5ca6799a4253f9edd759d2699d
Tadanasi <71709272+shiwusa@users.noreply.github.com>
2021-06-05 02:57:13 +0300

    bot refactor

--- js/bot.js
+++ js/bot.js
@@ -36,6 +36,18 @@ class Bot {
       else this.onPlayerAttacked();
     }, 2000);
   }
+  isCellExist(x, y) {
+    return  (GameEnviroment.Cells[PlayerType.Player1][x] && 
+      GameEnviroment.Cells[PlayerType.Player1][x][y] && 
+      GameEnviroment.Cells[PlayerType.Player1][x][y].cellType < 4);
+  }
+  setCoordinates(x, y, reset = false) {
+    if (reset) this.lastAttacked.coords = [];
+    this.lastAttacked.coords.push({
+      x: x,
+      y: y
+    });
+  }
   randomHit() {
     let x, y, hit;
     this.nextattack = null;
@@ -43,120 +55,116 @@ class Bot {
       x = Math.floor(Math.random() * CFG.gridSize);
       y = Math.floor(Math.random() * CFG.gridSize);
       if (
-        GameEnviroment.Cells[PlayerType.Player1][x][y] &&
-        GameEnviroment.Cells[PlayerType.Player1][x][y].cellType < 4
+        this.isCellExist(x, y)
       )
         break; //checking if we hadn't already shotted
     }
     hit = GameEnviroment.shot(x, y, PlayerType.Player1);
-    if (hit === 'Damaged')
+    if (hit === 'Damaged') {
       this.lastAttacked = {
-        coords: [[x, y]],
-        vector: null,
+        vector: null
       };
+      this.setCoordinates(x, y, true);
+    }
     return hit;
   }
   vectorHit(PrevCoords) {
     let x, y, anvector, hit;
-    const len = PrevCoords.length;
+    const lastIndex = PrevCoords.length - 1;
+    const firstShot = PrevCoords[0];
+    const lastShot = PrevCoords[lastIndex];
+    const beforeLastShot = PrevCoords[lastIndex - 1];
     switch (this.lastAttacked.vector) {
     case 'Vertical': {
       anvector = 'Horizontal';
-      x = PrevCoords[len - 1][0];
-      if (!PrevCoords[len - 2]) {
-        y = (PrevCoords[0][1] - 1);
-        if (!GameEnviroment.Cells[PlayerType.Player1][x][y])
-          y = (PrevCoords[0][1] + 1);
-      } else if ((PrevCoords[len - 1][1] + 1) == PrevCoords[len - 2][1]) y = (PrevCoords[len - 1][1] - 1);
-      else y = (PrevCoords[len - 1][1] + 1);
+      x = lastShot.x;
+      if (!beforeLastShot) {
+        y = (firstShot.y - 1);
+        if (!this.isCellExist(x, y))
+          y = (firstShot.y + 1);
+      } else if ((lastShot.y + 1) == beforeLastShot.y) y = (lastShot.y - 1);
+      else y = (lastShot.y + 1);
       break;
     }
     case 'Horizontal': {
       anvector = 'Vertical';
-      if (!PrevCoords[len - 2]) {
-        x = (PrevCoords[0][0] - 1);
-        if (!GameEnviroment.Cells[PlayerType.Player1][x])
-          x = (PrevCoords[0][0] + 1);
+      y = lastShot.y;
+      if (!beforeLastShot) {
+        x = (firstShot.x - 1);
+        if (!this.isCellExist(x,y))
+          x = (firstShot.x + 1);
       } else
-      if ((PrevCoords[len - 1][0] + 1) == PrevCoords[len - 2][0])
-        x = (PrevCoords[len - 1][0] - 1);
-      else x = (PrevCoords[len - 1][0] + 1);
-      y = PrevCoords[len - 1][1];
+      if ((lastShot.x + 1) == beforeLastShot.x)
+        x = (lastShot.x - 1);
+      else x = (lastShot.x + 1);
       break;
     }
     }
-    if (!GameEnviroment.Cells[PlayerType.Player1][x] ||
-      !GameEnviroment.Cells[PlayerType.Player1][x][y]) {
+    if (!this.isCellExist(x, y)) {
       if (this.missstate > 1) this.lastAttacked.vector = anvector;
       else this.missstate++;
-      this.lastAttacked.coords = [PrevCoords[0]];
+      this.lastAttacked.coords = [firstShot];
       hit = 'skips round';
     } else {
       hit = GameEnviroment.shot(x, y, PlayerType.Player1);
       switch (hit) {
       case 'Damaged':
-        this.lastAttacked.coords.push([x, y]);
+        this.setCoordinates(x, y);
         break;
       case 'Error':
-        if (PrevCoords.length > 1)
-          this.nextattack = [ (PrevCoords[0][0] - (x - PrevCoords[len - 1][0])),
-            (PrevCoords[0][1] - (y - PrevCoords[len - 1][1])) ];
-        else this.nextattack = [ (PrevCoords[len - 1][0] - (x - PrevCoords[len - 1][0])),
-          (PrevCoords[len - 1][1] - (y - PrevCoords[len - 1][1])) ];
+        if (lastIndex)
+          this.nextattack = [ (firstShot.x - (x - lastShot.x)),
+            (firstShot.y - (y - lastShot.y)) ];
+        else this.nextattack = [ (lastShot.x - (x - lastShot.x)),
+          (lastShot.y - (y - lastShot.y)) ];
         hit = 'Missed';
-        break;
-      default:
         break;
       }
     }
     return hit;
   }
   noneVectorHit(PrevCoords) {
+    const firstShot = PrevCoords[0];
     let potentialVector, x, y, hit;
+    const randomChoise = () => Math.random(2) >= 0.8;
     while (true) {
-      if (Math.random(2) >= 0.5) {
+      if (randomChoise()) {
         potentialVector = 'Vertical';
-        if (Math.random(2) >= 0.5) y = PrevCoords[0][1] + 1;
-        else y = PrevCoords[0][1] - 1;
-        x = PrevCoords[0][0];
+        if (randomChoise()) y = firstShot.y + 1;
+        else y = firstShot.y - 1;
+        x = firstShot.x;
       } else {
         potentialVector = 'Horizontal';
-        if (Math.random(2) >= 0.5) x = PrevCoords[0][0] + 1;
-        else x = PrevCoords[0][0] - 1;
-        y = PrevCoords[0][1];
+        if (randomChoise()) x = firstShot.x + 1;
+        else x = firstShot.x - 1;
+        y = firstShot.y;
       }
       if (
-        GameEnviroment.Cells[PlayerType.Player1][x] &&
-        GameEnviroment.Cells[PlayerType.Player1][x][y] &&
-        GameEnviroment.Cells[PlayerType.Player1][x][y].cellType < 4
+        this.isCellExist(x, y)
       )
         break;
     }
 
     hit = GameEnviroment.shot(x, y, PlayerType.Player1);
-    switch (hit) {
-    case 'Damaged':
-      this.lastAttacked.coords.push([x, y]);
+    if ( hit == 'Damaged' ) {
+      this.setCoordinates(x, y);
       this.lastAttacked.vector = potentialVector;
-      break;
-    case 'Aimed':
-      break;
-    default:
-      this.lastAttacked.coords = [this.lastAttacked.coords[0]];
+    }
+    else if ( hit != 'Aimed' ) {
+      this.lastAttacked.coords = [firstShot];
       hit = 'Missed';
-      break;
     }
     return hit;
   }
   botAttack() {
-    let hit, x, y;
+    let hit;
     if (this.nextattack && this.lastAttacked) {
       const temporary = this.nextattack;
       this.nextattack = null;
-      hit = GameEnviroment.shot(temporary[0], temporary[1], PlayerType.Player1);
+      hit = GameEnviroment.shot(temporary.x, temporary.y, PlayerType.Player1);
       if (hit == 'Damaged') {
-        const first = this.lastAttacked.coords[0];
-        this.lastAttacked.coords = [ first, temporary ];
+        const firstShot = this.lastAttacked.coords[0];
+        this.lastAttacked.coords = [ firstShot, temporary ];
       }
     } else if (!this.lastAttacked) {
       hit = this.randomHit();
